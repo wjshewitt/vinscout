eca21ed7d5222bdcac5cc1bcbb259b37065d39cb
William Hewitt <wjshewitt1@gmail.com>
2025-08-20 19:07:40 +0000

    Replace with this <script src='https://storage.ko-fi.com/cdn/scripts/ove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Header } from '@/components/layout/header';
 import { Providers } from '@/components/layout/providers';
 import { Footer } from '@/components/layout/footer';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Vinchaser',
@@ -30,6 +31,17 @@ export default function RootLayout({
           <Footer />
           <Toaster />
         </Providers>
+        <Script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js' />
+        <Script id="kofi-widget">
+          {`
+            kofiWidgetOverlay.draw('autofind', {
+              'type': 'floating-chat',
+              'floating-chat.donateButton.text': 'Support me',
+              'floating-chat.donateButton.background-color': '#00b9fe',
+              'floating-chat.donateButton.text-color': '#fff'
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
