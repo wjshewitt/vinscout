a5df32c069245d84667592e911f3ae018f0a9c69
William Hewitt <wjshewitt1@gmail.com>
2025-08-20 13:20:38 +0000

    ReferenceError: useFormContext is not defined     at LocationPicker (htt

--- src/components/vehicle-report-form.tsx
+++ src/components/vehicle-report-form.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useForm, useWatch } from 'react-hook-form';
+import { useForm, useWatch, useFormContext } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
