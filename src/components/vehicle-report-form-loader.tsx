2b538656ec44e7a86ed98d2522ccec8d0522a4b1
William Hewitt <hewittjswill@gmail.com>
2025-08-21 13:13:14 +0000

    Report Vehicle very slow too

--- src/components/vehicle-report-form-loader.tsx
+++ src/components/vehicle-report-form-loader.tsx
@@ -3,10 +3,11 @@
 import dynamic from 'next/dynamic';
 import { Skeleton } from '@/components/ui/skeleton';
 
+// By removing `ssr: false`, we allow Next.js to pre-render the static parts of the form on the server.
+// The interactive client-side logic will still hydrate in the browser, but the initial HTML is delivered faster.
 const VehicleReportForm = dynamic(
   () => import('@/components/vehicle-report-form').then((mod) => mod.VehicleReportForm),
   {
-    ssr: false,
     loading: () => <div className="p-8"><Skeleton className="h-[500px] w-full" /></div>,
   }
 );
